d7bc1a142e8da40f849a8880f9e23e90e5b860e5
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-10-02 22:06:52 -0300

    setting up eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+    'airbnb',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/prop-types': 'off', // Disable prop-types as we use TypeScript for type checking
+    'react/react-in-jsx-scope': 'off', // needed for NextJS's jsx without react import
+  },
+};
