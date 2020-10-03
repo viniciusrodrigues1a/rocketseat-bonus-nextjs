7d3913912adbef7725378be14527012c2db83e45
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-10-03 01:35:58 -0300

    changing eslint settings and rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 module.exports = {
   env: {
     browser: true,
@@ -23,5 +24,48 @@ module.exports = {
     'prettier/prettier': 'error',
     'react/prop-types': 'off', // Disable prop-types as we use TypeScript for type checking
     'react/react-in-jsx-scope': 'off', // needed for NextJS's jsx without react import
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'import/prefer-default-export': 'off',
+    'import/extensions': 'off',
+    'import/no-unresolved': 'error',
+    'react/jsx-props-no-spreading': 'off',
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      // use <root>/tsconfig.json
+      typescript: {
+        // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+        alwaysTryTypes: true,
+      },
+
+      // use <root>/path/to/folder/tsconfig.json
+      typescript: {
+        project: 'path/to/folder',
+      },
+
+      // Multiple tsconfigs (Useful for monorepos)
+
+      // use a glob pattern
+      typescript: {
+        project: 'packages/*/tsconfig.json',
+      },
+
+      // use an array
+      typescript: {
+        project: [
+          'packages/module-a/tsconfig.json',
+          'packages/module-b/tsconfig.json',
+        ],
+      },
+
+      // use an array of glob patterns
+      typescript: {
+        project: ['packages/*/tsconfig.json', 'other-packages/*/tsconfig.json'],
+      },
+    },
   },
 };
