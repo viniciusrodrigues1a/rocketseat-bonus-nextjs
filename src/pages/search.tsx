5a2a28647754cb19b5820e1e561abf6de82da31f
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-10-16 18:21:12 -0300

    styling search page

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,4 +1,4 @@
-import { useState, FormEvent } from 'react';
+import { useState, useMemo, FormEvent } from 'react';
 import { GetServerSideProps } from 'next';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
@@ -6,6 +6,7 @@ import Prismic from 'prismic-javascript';
 import PrismicDOM from 'prismic-dom';
 import { client } from '@/lib/prismic';
 import { Document } from 'prismic-javascript/types/documents';
+import { Box, Flex, IconButton, InputGroup, InputRightElement, Input } from '@chakra-ui/core';
 
 interface SearchProps {
   searchResults: Document[];
@@ -14,6 +15,7 @@ interface SearchProps {
 export default function Search({ searchResults }: SearchProps) {
   const router = useRouter();
   const [search, setSearch] = useState('');
+  const searchLength = useMemo(() => search.length, [search]);
 
   function handleSearch(e: FormEvent) {
     e.preventDefault();
@@ -22,24 +24,63 @@ export default function Search({ searchResults }: SearchProps) {
   }
 
   return (
-    <div>
-      <form onSubmit={handleSearch}>
-        <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} />
-        <button type="submit">Search</button>
-      </form>
+    <Box>
+      <Flex justify="center" align="center">
+        <form onSubmit={handleSearch}>
+          <Flex align="center">
+            <InputGroup>
+              <Input
+                type="text"
+                placeholder="Procure por um produto"
+                backgroundColor="gray.700"
+                variant="filled"
+                value={search}
+                onChange={(e: FormEvent) => setSearch((e.target as HTMLInputElement).value)}
+                _hover={{
+                  backgroundColor: 'gray.600',
+                }}
+                _active={{}}
+                _focus={{
+                  backgroundColor: 'purple.700',
+                }}
+                _placeholder={{
+                  color: 'gray.100',
+                }}
+              />
+              <InputRightElement
+                children={
+                  <IconButton
+                    type="submit"
+                    aria-label="Search for products"
+                    icon="search"
+                    variant="ghost"
+                    outline="none"
+                    color={searchLength >= 3 ? 'purple.500' : 'white'}
+                    _hover={{}}
+                    _active={{}}
+                    _focus={{}}
+                  />
+                }
+              />
+            </InputGroup>
+          </Flex>
+        </form>
+      </Flex>
 
-      <ul>
-        {searchResults.map((product) => {
-          return (
-            <li key={product.id}>
-              <Link href={`/catalog/products/${product.uid}`}>
-                <a>{PrismicDOM.RichText.asText(product.data.title)}</a>
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+      <Flex justify="center" align="center">
+        <ul>
+          {searchResults.map((product) => {
+            return (
+              <li key={product.id}>
+                <Link href={`/catalog/products/${product.uid}`}>
+                  <a>{PrismicDOM.RichText.asText(product.data.title)}</a>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </Flex>
+    </Box>
   );
 }
 
